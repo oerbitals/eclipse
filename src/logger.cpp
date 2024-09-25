1d1b6b0fb6f31bec934c5847dd95b5547f9d3a24
Tom <oerushia@gmail.com>
2024-09-25 14:37:15 +0100

    Implement singleton and build steps

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,16 +1,7 @@
 #include <colours.h>
 #include <logger.h>
 
-#define FILE_INFO() __FILE__, __LINE__
-
-string Logger::fileInformation(string file, int line) {
-    size_t lastSlash = file.find_last_of("/\\");
-    if (lastSlash != string::npos) {
-        file = file.substr(lastSlash + 1);
-    }
-
-    return Colour::dim("[" + file + ":" + std::to_string(line) + "]");
-}
+Logger* Logger::instance = nullptr;
 
 string Logger::timestamp() {
     auto now = std::chrono::system_clock::now();
@@ -80,33 +71,32 @@ Colour::Code Logger::getColour(LogLevel& logLevel) {
     }
 }
 
-void Logger::write(string& message, LogLevel logLevel, string file, int line) {
+void Logger::write(string& message, LogLevel logLevel) {
     std::ostringstream oss;
     Colour::Code colour = getColour(logLevel);
     string mainMessage = Colour::colour(message, colour);
     string tag = Colour::colour(getTag(logLevel), colour);
-    string fileInfo = Colour::dim(fileInformation(file, line));
     
-    oss << padRight(timestamp(), 30) << padRight(fileInfo, 33) << padRight("[" + tag + "]:", 20) << mainMessage << " " << std::endl;
+    oss << padRight(timestamp(), 30) << padRight("[" + tag + "]:", 20) << mainMessage << " " << std::endl;
     std::cout << oss.str();
 }
 
-void Logger::silly(string message, string file, int line) {
-    write(message, LogLevel::SILLY, file, line);
+void Logger::silly(string message) {
+    write(message, LogLevel::SILLY);
 }
 
-void Logger::info(string message, string file, int line) {
-    write(message, LogLevel::INFO, file, line);
+void Logger::info(string message) {
+    write(message, LogLevel::INFO);
 }
 
-void Logger::warning(string message, string file, int line) {
-    write(message, LogLevel::WARNING, file, line);
+void Logger::warning(string message) {
+    write(message, LogLevel::WARNING);
 }
 
-void Logger::error(string message, string file, int line) {
-    write(message, LogLevel::ERROR, file, line);
+void Logger::error(string message) {
+    write(message, LogLevel::ERROR);
 }
 
-void Logger::fatal(string message, string file, int line) {
-    write(message, LogLevel::FATAL, file, line);
+void Logger::fatal(string message) {
+    write(message, LogLevel::FATAL);
 }
